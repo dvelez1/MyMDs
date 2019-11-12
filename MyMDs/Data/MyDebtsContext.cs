d23843637138531fe0686c9ee9d3f2233788a518
Dennis Velez <velez.dennis0@outlook.com>
2019-11-12 05:58:06 -0400

    Add project files.

---
+++ MyMDs/Data/MyDebtsContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyMDs.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyMDs.Data
+{
+ 
+
+
+        public class MyDebtsContext : DbContext
+        {
+            public MyDebtsContext(DbContextOptions<MyDebtsContext> options) : base(options)
+            {
+            }
+
+            public DbSet<DebtsMaster> debtsMasters { get; set; }
+            public DbSet<MyDebts> myDebts { get; set; }
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<DebtsMaster>().ToTable("DebtsMaster");
+            modelBuilder.Entity<MyDebts>().ToTable("MyDebts");
+           
+        }
+    }
+    
+}
---
+++ MyMDs/Models/MyDebts.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyMDs.Models
+{
+    public class MyDebts
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int MyDebtsId { get; set; }
+
+        public int Mas_debts_Id { get; set; }
+
+        [Range(1, 12, ErrorMessage = "The Month should be between 1 and 12")]
+        public int Month { get; set; }
+
+        [Range(2019, 2050, ErrorMessage = "The year should be between Current Year to Next Year")]
+         public int Year { get; set; }
+
+        [Required]
+        [Range(1, 12, ErrorMessage = "The Number should be between 1 = First Quincenal and 2 = Second Quincenal")]
+        public int Quincenal { get; set; } // 1 or 2
+
+        [DataType(DataType.Currency)]
+        public decimal DebtQuantity { get; set; }
+
+        
+
+    }
+}
